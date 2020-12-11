d6623212615503e1bd5edc854661c9f4f6a68faf
houfei <fei.hou@wenba100.com>
2020-12-11 14:57:16 +0800

    减少未安装的依赖

--- src/main.js
+++ src/main.js
@@ -6,11 +6,6 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
-import VueEditor from "vue2-editor"
-
-import FormMaking from 'form-making'
-import 'form-making/dist/FormMaking.css'
-
 import '@/styles/index.scss' // global css
 
 import App from './App'
@@ -35,10 +30,6 @@ if (process.env.NODE_ENV === 'production') {
 Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
-
-Vue.use(VueEditor)
-
-Vue.use(FormMaking, {lang: 'zh-CN'}) // en-US
 
 Vue.config.productionTip = false
 
