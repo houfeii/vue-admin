55afc7cee08a326e060bec08f03858d89f1eb261
houfeii <39523094+houfeii@users.noreply.github.com>
2020-12-11 07:27:49 +0000

    Deploying to gh-pages from  @ d6623212615503e1bd5edc854661c9f4f6a68faf 🚀

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: 'vue-element 后台',
+
+  /**
+   * @type {boolean} true | false
+   * @description 是否固定头部
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description 是否固定侧边栏的logo
+   */
+  sidebarLogo: true
+}
