62073f217225fb4d3b25faef587c02c0afbde937
houfeii <39523094+houfeii@users.noreply.github.com>
2020-12-11 07:33:52 +0000

    Deploying to gh-pages from  @ 178da12be9d4c1f836b80640985749415ac52961 🚀

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-// https://github.com/michael-ciniawsky/postcss-load-config
-
-module.exports = {
-  'plugins': {
-    // 要编辑目标浏览器：请使用中的“browserslist”字段包.json
-    'autoprefixer': {}
-  }
-}
--- src/store/getters.js
+++
@@ -1,9 +0,0 @@
-const getters = {
-  sidebar: state => state.app.sidebar,
-  device: state => state.app.device,
-  token: state => state.user.token,
-  avatar: state => state.user.avatar,
-  formJson: state => state.formMaking.formJson,
-  name: state => state.user.name
-}
-export default getters
